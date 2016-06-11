4025ecf7a9b0fe17c156b0bd24df1b8f08f8c1d6
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-06-11 23:53:00 +0900

    data structure changed

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/initial', function(req, res, next) {
-	fs.readFile('./src_adjust.json', 'utf8', function (err, text) {
+	fs.readFile('./src_data.json', 'utf8', function (err, text) {
 		if(err){
 			console.log(err);
 			res.send(err);
@@ -38,7 +38,7 @@ router.post('/push_QA', function(req, res, next) {
 		return;
 	}
 	push_data_to_global_text_array(req, res, next, req.body.quetion, req.body.answer_title,req.body.answer_body );
-
+	store_data_to_src_file()
 });
 
 
@@ -55,6 +55,29 @@ function push_data_to_global_text_array(req, res, next, quetion_val , answer_tit
 	res.send(obj)
 }
 
+function store_data_to_src_file(){
+	var data_array = new Array();
+	for(var i=0; i< global_text_array.length; i++){
+		var obj = {
+			Question: global_text_array[i].Question,
+			Answer: global_text_array[i].Answer,
+			id: i
+		}
+		data_array.push(obj);
+	}
+	var text_data = JSON.stringify(data_array);
+
+	fs.writeFile('src_data.json', text_data , function (err) {
+		if(!err){
+			console.log(err)
+			return;
+		}else{
+			console.log("succeed to save dta in src_data.json")
+		}
+	});
+
+
+}
 
 
 
@@ -69,7 +92,7 @@ router.get('/output', function(req, res, next) {
 		var obj = {
 			add: {
 				doc: {
-					id: global_text_array[i].id,
+					id: i,
 					body: global_text_array[i].Answer.body,
 					title: global_text_array[i].Answer.title
 				}
@@ -102,7 +125,7 @@ function output_csv(req, res, next){
 	var output_csv = "";
 	for(var i=0; i< global_text_array.length; i++){
 		var output_line =  '"' + global_text_array[i].Question + '",' + 
-						   '"' + global_text_array[i].id  + '",' + 
+						   '"' + i  + '",' + 
 						   '"4"\r\n';
 		output_csv = output_csv + output_line;
 	}
